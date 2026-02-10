c98e3fda92a25765b119ebf451e22e2ecfc007ae
Carlossnogueira <carlosnogueira372@gmail.com>
2026-02-10 07:49:40 -0300

    fix(prisma-service): conexão entre o client e o banco

--- src/shared/prisma.service.ts
+++ src/shared/prisma.service.ts
@@ -1,14 +1,24 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "generated/prisma/client";
-
+import { PrismaClient } from "src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import 'dotenv/config'
+import { env } from "prisma/config";
 
 @Injectable()
 export class PrismaService extends PrismaClient
     implements OnModuleInit, OnModuleDestroy {
 
+    constructor() {
+        const adapter = new PrismaPg({ connectionString: env("DATABASE_URL") })
+        super({ adapter })
+    }
 
     async onModuleInit() {
-        await this.$connect()
+        try {
+            await this.$connect()
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     async onModuleDestroy() {
